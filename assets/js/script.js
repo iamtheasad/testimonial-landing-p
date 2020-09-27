445af50e08f1a4359018803aa6b5e101b38f5c1e
iamtheasad <iamtheasad@gmail.com>
2020-09-27 18:34:34 +0600

    featured testimonial section added with slick carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,42 @@
 
    $(document).ready(function () {
 
-      // Place All Code Here
+       /*Logo Slider*/
+       $('.logo_carousel').slick({
+           dots: false,
+           slidesToShow: 5,
+           slidesToScroll: 1,
+           infinite: true,
+           autoplay: false,
+           pauseOnHover: false,
+           arrows: true,
+           prevArrow: '<button type="button" class="slick-prev"><span class="arrow_prev_bg"></span></button>',
+           nextArrow: '<button type="button" class="slick-next"><span class="arrow_next_bg"></span></button>',
+           autoplaySpeed: 2000,
+           responsive: [{
+               breakpoint: 1024,
+               settings: {
+                   arrows: false,
+                   slidesToShow: 4,
+               }
+           },
+               {
+                   breakpoint: 600,
+                   settings: {
+                       arrows: false,
+                       slidesToShow: 3,
+                   }
+               },
+               {
+                   breakpoint: 480,
+                   settings: {
+                       arrows: false,
+                       slidesToShow: 2,
+                   }
+               }
+           ]
+       });
       
    });
-
  })();
  
